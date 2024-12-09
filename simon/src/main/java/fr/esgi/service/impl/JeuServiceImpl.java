6444504d8323a3c1275080954c6931e03b93984d
matysauvinet <mauvinet@mstdudioweb.fr>
2024-12-09 17:52:32 +0100

    Ajout dossier test

---
+++ simon/src/main/java/fr/esgi/service/impl/JeuServiceImpl.java
@@ -0,0 +1,4 @@
+package fr.esgi.service.impl;
+
+public class JeuServiceImpl {
+}
---
+++ simon/src/test/java/fr/esgi/service/impl/SonServiceImplTest.java
@@ -0,0 +1,6 @@
+package fr.esgi.service.impl;
+
+class SonServiceImplTest {
+}
+
+//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
