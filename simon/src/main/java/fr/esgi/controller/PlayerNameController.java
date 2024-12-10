733f5508f316c6c8c76171f06341f7339ac94619
Nicolas1904 <nicolas.ryckelynck@epitech.eu>
2024-12-10 16:39:09 +0100

    Player name

--- simon/src/main/java/fr/esgi/controller/PlayerNameController.java
+++ simon/src/main/java/fr/esgi/controller/PlayerNameController.java
@@ -3,11 +3,16 @@ package fr.esgi.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import java.util.ArrayList;
+import java.util.List;
+import fr.esgi.business.Joueur;
 
 public class PlayerNameController {
 
+    private List<Joueur> joueurs = new ArrayList<>();
+
     @FXML
-    private VBox playerNameFields;
+    VBox playerNameFields;
 
     public void setNumberOfPlayers(int numberOfPlayers) {
         playerNameFields.getChildren().clear();
@@ -17,5 +22,26 @@ public class PlayerNameController {
             textField.setPromptText("Prénom du joueur " + i);
             playerNameFields.getChildren().add(textField);
         }
+    }
+
+    @FXML
+    public void savePlayerNames() {
+        joueurs.clear();
+        for (javafx.scene.Node node : playerNameFields.getChildren()) {
+            if (node instanceof TextField) {
+                TextField textField = (TextField) node;
+                String prenom = textField.getText();
+                if (!prenom.isEmpty()) {
+                    joueurs.add(new Joueur(prenom));
+                }
+            }
+        }
+        for (Joueur joueur : joueurs) {
+            System.out.println("Player: " + joueur.getPrenom());
+        }
+    }
+
+    public List<Joueur> getJoueurs() {
+        return joueurs;
     }
 }
