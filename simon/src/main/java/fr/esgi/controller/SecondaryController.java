86667ed25b370b8c3a89be50fc94bd184df0c250
Joris Noël <joris.noel12@gmail.com>
2025-01-04 14:09:53 +0100

    nombreJoueur

--- simon/src/main/java/fr/esgi/controller/SecondaryController.java
+++ simon/src/main/java/fr/esgi/controller/SecondaryController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
 
 public class SecondaryController {
 
