dc11d02a045d27cdee6e32168716d14ce905a9ba
Joris Noël <joris.noel12@gmail.com>
2025-01-15 20:39:14 +0100

    function btnRetour crédit + page crédit / CreditsController

--- simon/src/main/java/fr/esgi/controller/CreditsController.java
+++ simon/src/main/java/fr/esgi/controller/CreditsController.java
@@ -1,7 +1,29 @@
 package fr.esgi.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class CreditsController {
-    // Vous pouvez ajouter des méthodes ici si vous avez besoin de gérer des événements
+
+    @FXML
+    private Button btnRetour;  // Bouton retour
+
+    @FXML
+    public void initialize() {
+        // Action pour le bouton retour
+        btnRetour.setOnAction(event -> {
+            try {
+                // Charger la page principale
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/fr/esgi/primary.fxml"));
+                Stage stage = (Stage) btnRetour.getScene().getWindow();
+                stage.setScene(new Scene(loader.load()));
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
